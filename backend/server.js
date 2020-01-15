dcc2c31588179bfb0b0ccec09ba74aad47d743a9
Taleeb Midi <taleebmidi@gmail.com>
2020-01-15 00:03:45 -0500

    reorganize paassport and remove packages

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const exercisesRouter = require("./routes/exercises");
-const usersRouter = require("./routes/users");
-const adminRouter = require("./routes/admin");
-const LdapStrategy = require("passport-ldapauth");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+// Import packages
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+const session = require('express-session');
 
-require("dotenv").config();
+// Inclue .env configs
+require('dotenv').config();
 
-const defaultConfig = {
-  ldap: {
-    url: "",
-    bindDN: "",
-    bindPass: "",
-    searchBase: "",
-    searchFilter: ""
-  }
-};
-
-// LDAP Auth template in .env
-
+// init Express server
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(bodyParser.urlencoded({ extended: true }));
-passport.use(new LdapStrategy(OPTS));
 
-app.use(bodyParser.json());
-
-app.use(passport.initialize());
+// Passport Config
+require('./config/passport')(passport);
 
 app.use(cors());
 app.use(express.json());
 
+// DB Config
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useCreateIndex: true,
@@ -41,35 +27,67 @@ mongoose.connect(uri, {
   useUnifiedTopology: true
 });
 
+//Connect to Mongo
 const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
   connection.db.listCollections().toArray(function(err, collectionNames) {
     if (err) {
       console.log(err);
       return;
     } else {
-      if (collectionNames.find(({ name }) => name === "Configs")) {
-        console.log("Configs already exists");
+      if (collectionNames.find(({ name }) => name === 'Configs')) {
+        console.log('Configs already exists');
       } else {
-        console.log("init default config collections");
+        console.log('init default config collections');
         const initConfig = new Configs({
           ldap: defaultConfig.ldap
         });
 
         initConfig
           .save()
-          .then(console.log("Added init default configs"))
-          .catch(err => console.log("Error: " + err));
+          .then(console.log('Added init default configs'))
+          .catch(err => console.log('Error: ' + err));
       }
     }
   });
 });
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRouter);
-app.use("/admin", adminRouter);
+// Bodyparser
+app.use(express.urlencoded({ extended: true }));
 
+app.use(passport.initialize());
+
+// Expression session middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars:
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/exercises', require('./routes/exercises'));
+app.use('/users', require('./routes/users'));
+app.use('/admin', require('./routes/admin'));
+
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
