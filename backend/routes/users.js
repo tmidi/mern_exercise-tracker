e1805ff4b16581e3fba36825ae1e045c98520476
Taleeb Midi <taleebmidi@gmail.com>
2020-01-15 00:06:34 -0500

    now ldap auth is working

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,41 @@
-const router = require("express").Router();
-let User = require("../models/user.model");
+const router = require('express').Router();
+const User = require('../models/user.model');
+const passport = require('passport');
 
-router.route("/").get((req, res) => {
+router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route("/add").post((req, res) => {
+router.route('/add').post((req, res) => {
   const username = req.body.username;
 
   const newUser = new User({ username });
 
   newUser
     .save()
-    .then(() => res.json("User added"))
-    .catch(err => res.status(400).json("Error: " + err));
+    .then(() => res.json('User added'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('ldapauth', { session: false }, function(
+    err,
+    user,
+    info
+  ) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.send({
+        success: false,
+        message: `authentication failed ${info.message}`
+      });
+    }
+    return res.send({ success: true, message: 'authentication succeeded' });
+  })(req, res, next);
 });
 
 module.exports = router;
